75eb0bcfbe963dfa7d0000b6be3ee214e9c2203f
siddiquirock <aamilsiddiqui2015@gmail.com>
2025-07-13 17:23:26 +0530

    Update main.tf
    123

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 
-resource "azurerm_resource_group" "resource_group" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
+resource "azurerm_resource_group" "resource_group11" {
+  name     = var.resource_group_name11
+  location = var.resource_group_location11
 }
